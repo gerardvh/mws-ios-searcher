6bf03b0d91e714b89ee58f9c6766f28b2a8eaa2c
Gerard Van Halsema <gerardvh@me.com>
2015-11-09 15:36:41 -0500

    Switch to a protocol.

--- SLMobile/DetailViewController.swift
+++ SLMobile/DetailViewController.swift
@@ -8,38 +8,10 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+protocol DetailViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    var detailItem: Any? { get set }
 
-
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    func configureView()
 }
 
