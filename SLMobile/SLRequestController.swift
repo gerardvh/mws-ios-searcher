0e83f5db46dce06efc8bab8e9d7c383a4513a33c
Gerard Van Halsema <gerardvh@me.com>
2015-11-09 16:59:32 -0500

    Refactor makeRequest to be more clear and add comments.

--- SLMobile/SLRequestController.swift
+++ SLMobile/SLRequestController.swift
@@ -45,44 +45,48 @@ class SLRequestController: NSObject, NSURLSessionDelegate, NSURLSessionDataDeleg
     // Basically I want to be able to either pass in a String as a query (which uses the default setup),
     // or I can pass in a Dictionary when I set up the different filter options to specify what I'm searching.
     
-    func makeRequest(searchTerm: String, completionHandler handler: ([Computer]) -> Void) {
-        
+    func makeRequest(searchTerm: String, completionHandler handler: ([NSDictionary]) -> Void) {
         let request = self.configureRequest(searchTerm)
         let singleSession = getSession()
         
+        // Stop the running data task if we enter this method while there is still a data task pending
+        // this way we don't get the results for an old data task
         if ((self.dataTask) != nil) {
             Debug.log("Canceling existing dataTask")
             self.dataTask?.cancel()
         }
-        
+        // After making sure that we don't already have an active request, continue on with the dataTask
         self.dataTask = singleSession.dataTaskWithRequest(request) { (data, response, error) -> Void in
             // Verify that we have not already canceled this dataTask, otherwise return early.
-            guard data != nil
-                else { Debug.log("Data was nil"); return }
-            Debug.log("Got data")
-            var computers = [Computer]()
-            do {
-                // Already know that data will not be nil
-                let jsonData = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-                if let
-                    jsonResponse = jsonData as? Dictionary<String, AnyObject>,
-                    result       = jsonResponse["result"] as? [NSDictionary] {
-                        for element in result {
-                            computers.append(Computer(dictionary: element))
-                        }
-                        // Send the new array back to our handler.
-                        handler(computers)
-                } else {
-                    Debug.log("Maybe ServiceLink error")
+            guard let dataUnwrapped = data
+                else {
+                    Debug.log("Data was nil")
+                    return
                 }
-            } catch {
-                Debug.log("Error converting json data: \(error)")
+            // Guaranteed that we have data at this point
+            Debug.log("Got data")
+            // Use guard to return early and show our `else` cases near the code that triggers them
+            guard let
+                jsonData = try? NSJSONSerialization.JSONObjectWithData(dataUnwrapped, options: .AllowFragments),
+                jsonResponse = jsonData as? Dictionary<String, AnyObject>
+                else {
+                    Debug.log("Unable to parse JSON from data: \(dataUnwrapped)")
+                    return
+                }
+            // Results from ServiceLink are arrays of dictionaries, here we know we have a result from SL
+            if let result = jsonResponse["result"] as? [NSDictionary] {
+                // Send the Array back to our handler
+                handler(result)
+            }
+            // When a query doesn't match anything, ServiceLink sends back an `error` dictionary
+            if let error = jsonResponse["error"] as? NSDictionary {
+                Debug.log("SL Error: \(error["message"])")
             }
         }
         self.dataTask?.resume()
     }
     
-    func makeSpecificRequest(searchDict: Dictionary<String, String>) {
+    func makeSpecificRequest(searchDict: Dictionary<String, String>, completionHandler handler: ([NSDictionary]) -> Void) {
         fatalError("makeSpecificRequest not implemented yet.")
     }
     
@@ -94,20 +98,23 @@ class SLRequestController: NSObject, NSURLSessionDelegate, NSURLSessionDataDeleg
     private func configureSession() -> NSURLSession {
         let config                   = NSURLSessionConfiguration.defaultSessionConfiguration()
         config.HTTPAdditionalHeaders = ["Authorization": SLKey.API.authString, "Accept": "application/json"]
-        
-        let session = NSURLSession(configuration: config)
-        return session
+        return NSURLSession(configuration: config)
         
     }
     
     private func configureRequest(searchTerm: String) -> NSURLRequest {
+        return configureRequest(searchTerm, queryItems: defaultQueryItems(searchTerm))
+    }
+    
+    private func configureRequest(searchTerm: String, queryItems: [NSURLQueryItem]) -> NSURLRequest {
         let urlComponents = SLKey.ConfigurationItem.urlComponents
-        urlComponents.queryItems = defaultQueryItems(searchTerm)
+        urlComponents.queryItems = queryItems
         return NSURLRequest(URL: urlComponents.URL!, cachePolicy: .ReturnCacheDataElseLoad, timeoutInterval: 10.0)
     }
     
-    
 }
+
+// MARK: - Private Dictionary extension
 
 private extension Dictionary {
     func asSLQueryString(separator separator: String = SLKey.QuerySeparators.Or) -> String {
